24004cc23fd5ba32bfbf25a7641b96a4e501d01d
MSI-Gl62 <karn939.n@gmail.com>
2018-02-21 12:43:32 +0700

    21/2/2556 setup -setup UiExtension -network -setfacebookLogin

--- app/src/main/java/com/example/msi_gl62/co_work_android_uset/ui/home/MainActivity.kt
+++ app/src/main/java/com/example/msi_gl62/co_work_android_uset/ui/home/MainActivity.kt
@@ -1,6 +1,7 @@
-package com.example.msi_gl62.co_work_android_uset
+package com.example.msi_gl62.co_work_android_uset.ui.home
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.example.msi_gl62.co_work_android_uset.R
 
 class MainActivity : AppCompatActivity() {
 
