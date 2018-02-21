d5f665d0fbec61021705eeaeb56f8824eacf80e0
MSI-Gl62 <karn939.n@gmail.com>
2018-02-21 11:15:46 +0700

    test commit dev to day 21/2/2556

---
+++ app/src/main/java/com/example/msi_gl62/co_work_android_uset/testActivity.kt
@@ -0,0 +1,12 @@
+package com.example.msi_gl62.co_work_android_uset
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class testActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test)
+    }
+}
