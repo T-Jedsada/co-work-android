4dd0043aafb1410e72473349b62f7f0a85103674
fluke <flukebevil@gmail.com>
2018-02-22 17:26:17 +0700

    fixed travis

--- app/src/main/java/com/example/msigl62/coworkandroiduset/ui/home/MainActivity.kt
+++ app/src/main/java/com/example/msigl62/coworkandroiduset/ui/home/MainActivity.kt
@@ -2,7 +2,7 @@ package com.example.msigl62.coworkandroiduset.ui.home
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import com.example.msigl62.coworkandroiduset.R
+import com.example.msi_gl62.co_work_android_uset.R
 
 class MainActivity : AppCompatActivity() {
 
