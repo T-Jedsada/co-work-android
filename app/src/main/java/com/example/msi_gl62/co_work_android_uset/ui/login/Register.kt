41851cefc676fa1dfc1eeed40642368f4e69b195
MSI-Gl62 <karn939.n@gmail.com>
2018-02-22 21:46:01 +0700

    commit 22/2/2556 -editfacebook

--- app/src/main/java/com/example/msi_gl62/co_work_android_uset/ui/login/Register.kt
+++ app/src/main/java/com/example/msi_gl62/co_work_android_uset/ui/login/Register.kt
@@ -3,17 +3,22 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View.GONE
 import android.widget.Toast
 import com.example.msi_gl62.co_work_android_uset.R
 import com.facebook.*
 import com.facebook.login.LoginManager
 import com.facebook.login.LoginResult
 import kotlinx.android.synthetic.main.activity_register.*
-import java.util.*
 import java.util.regex.Pattern
+import com.facebook.FacebookException
+import com.facebook.FacebookCallback
+import android.R.attr.data
+import android.app.Activity
+
 
 class Register : AppCompatActivity() {
-    val PICK_IMAGE = 1000
+    val PICK_IMAGE = 3000
     private var callbackManager: CallbackManager? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,6 +26,7 @@ class Register : AppCompatActivity() {
         setImageviewUser()
         setButtonNext()
         getDataFacebook()
+        LoginManager.getInstance().logOut()
 
     }
 
@@ -30,40 +36,40 @@ class Register : AppCompatActivity() {
             val email = edtEmail.text.toString()
             val matcher = Pattern.compile(validemail).matcher(email)
             if (matcher.matches()){ Toast.makeText(getApplicationContext(),"True",Toast.LENGTH_LONG).show(); }
-            else { Toast.makeText(getApplicationContext(),"Fail",Toast.LENGTH_LONG).show(); }
-        } }
+            else { Toast.makeText(getApplicationContext(),"Fail",Toast.LENGTH_LONG).show(); } } }
 
     private fun setImageviewUser() {
         imageView.setOnClickListener{
-            val intent = Intent()
-            intent.type = "image/*"
-            intent.action = Intent.ACTION_PICK
-            startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE)
-        } }
+        val intent = Intent()
+        intent.type = "image/*"
+        intent.action = Intent.ACTION_PICK
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE) }
+    }
 
     private fun getDataFacebook() {
 
         callbackManager = CallbackManager.Factory.create()
-        LoginManager.getInstance().registerCallback(callbackManager,
+        login_button.setReadPermissions("email")
+        login_button.registerCallback(callbackManager,
                 object : FacebookCallback<LoginResult> {
                     override fun onSuccess(loginResult: LoginResult) {
                         val params = Bundle()
-                        params.putString("fields", "id,email,name,gender")
+                        params.putString("fields", "id,name,gender,email")
                         GraphRequest(AccessToken.getCurrentAccessToken(), "me", params, HttpMethod.GET,
-                                GraphRequest.Callback { response ->
-                                    val data = response.jsonObject
+                                GraphRequest.Callback { response -> val data = response.jsonObject
                                     if (response != null) {
                                         try {
                                             val textname=data.getString("name")
                                             edtName.setText(textname)
                                             val textid=data.getString("id")
-                                            Log.e("textid","......"+textid)
                                             val textemail=data.getString("email")
-                                            edtName.setText(textemail)
+                                            edtEmail.setText(textemail)
                                         } catch (e: Exception) {
                                             e.printStackTrace() }
                                     }
                                 }).executeAsync()
+                        login_button.visibility = GONE
+                        textOR.visibility = GONE
                     }
                     override fun onCancel() {}
                     override fun onError(exception: FacebookException) {}
@@ -73,14 +79,16 @@ class Register : AppCompatActivity() {
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent) {
         callbackManager?.onActivityResult(requestCode, resultCode, data)
         super.onActivityResult(requestCode, resultCode, data)
-
             if (requestCode == PICK_IMAGE) {
                 val picUri = data.data
-                Log.e("twtetst","........"+picUri)
-                imageView.setImageURI(picUri)
-            }
-
+                imageView.setImageURI(picUri) }
     }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        LoginManager.getInstance().logOut()
+    }
+
 
 }
 
