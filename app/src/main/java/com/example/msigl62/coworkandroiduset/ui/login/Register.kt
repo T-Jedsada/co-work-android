d64bebbbafdbdd4eb5e40bc2e77e2715a9e3be7d
MSI-Gl62 <karn939.n@gmail.com>
2018-02-23 13:05:47 +0700

    commit 23/2/2556 -editfacebook

--- app/src/main/java/com/example/msigl62/coworkandroiduset/ui/login/Register.kt
+++ app/src/main/java/com/example/msigl62/coworkandroiduset/ui/login/Register.kt
@@ -6,13 +6,11 @@ import android.util.Log
 import android.view.View.GONE
 import android.widget.Toast
 import com.example.msi_gl62.co_work_android_uset.R
+import com.facebook.*
 import com.facebook.login.LoginManager
 import com.facebook.login.LoginResult
 import kotlinx.android.synthetic.main.activity_register.*
 import java.util.regex.Pattern
-import com.facebook.FacebookException
-import com.facebook.FacebookCallback
-import com.facebook.CallbackManager
 import java.util.*
 
 class Register : AppCompatActivity() {
@@ -21,12 +19,10 @@ class Register : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
-        setImageviewUser()
+        setImageViewUser()
         setButtonNext()
         getDataFacebook()
         LoginManager.getInstance().logOut()
-
-
     }
 
     private fun setButtonNext() {
@@ -34,15 +30,20 @@ class Register : AppCompatActivity() {
             val validEmail = "[a-zA-Z0-9\\+\\.\\_\\%\\-\\+]{1,256}" + "\\@" + "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,64}" + "(" + "\\." + "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,25}" + ")+"
             val email = edtEmail.text.toString()
             val matcher = Pattern.compile(validEmail).matcher(email)
-            if (matcher.matches()){ Toast.makeText(applicationContext,"True",Toast.LENGTH_LONG).show(); }
-            else { Toast.makeText(applicationContext,"Fail",Toast.LENGTH_LONG).show(); } } }
+            if (matcher.matches()) {
+                Toast.makeText(applicationContext, "True", Toast.LENGTH_LONG).show(); } else {
+                Toast.makeText(applicationContext, "Fail", Toast.LENGTH_LONG).show(); }
+        }
+    }
 
-    private fun setImageviewUser() {
-        imageView.setOnClickListener{
+    private fun setImageViewUser() {
+        imageView.setOnClickListener {
             val intent = Intent()
             intent.type = "image/*"
             intent.action = Intent.ACTION_PICK
-            startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE) } }
+            startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE)
+        }
+    }
 
     private fun getDataFacebook() {
         btnFacebook.setOnClickListener {
@@ -53,10 +54,23 @@ class Register : AppCompatActivity() {
                         override fun onSuccess(loginResult: LoginResult) {
                             btnFacebook.visibility = GONE
                             textOR.visibility = GONE
+                            val request = GraphRequest.newMeRequest(
+                                    loginResult.accessToken
+                            ) { `object`, _ ->   Log.e("CheckEmail", `object`.has("email").toString())
+                            val name = `object`.getString("name")
+                                edtName.setText(name)
+                                var email =`object`.getString("email")
+                                edtEmail.setText(email)
+                            }
+                            val parameters = Bundle()
+                            parameters.putString("fields", "id,name,link,email")
+                            request.parameters = parameters
+                            request.executeAsync()
                         }
                         override fun onCancel() {}
                         override fun onError(error: FacebookException) {}
-                    }) }
+                    })
+        }
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent) {
@@ -64,11 +78,14 @@ class Register : AppCompatActivity() {
         super.onActivityResult(requestCode, resultCode, data)
         if (requestCode == PICK_IMAGE) {
             val picUri = data.data
-            imageView.setImageURI(picUri) } }
+            imageView.setImageURI(picUri)
+        }
+    }
 
     override fun onDestroy() {
         super.onDestroy()
-        LoginManager.getInstance().logOut() }
+        LoginManager.getInstance().logOut()
+    }
 
 }
 
