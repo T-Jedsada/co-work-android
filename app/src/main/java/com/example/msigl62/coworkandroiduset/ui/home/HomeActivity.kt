39198903fc396bf8bfdbe0fad1339e48dabc42a9
MSI-Gl62 <karn939.n@gmail.com>
2018-02-24 15:28:17 +0700

    commit 24/2/2556

--- app/src/main/java/com/example/msigl62/coworkandroiduset/ui/home/HomeActivity.kt
+++ app/src/main/java/com/example/msigl62/coworkandroiduset/ui/home/HomeActivity.kt
@@ -1,13 +1,28 @@
 package com.example.msigl62.coworkandroiduset.ui.home
 
+import android.support.annotation.IdRes
+import android.widget.Toast
 import com.example.msi_gl62.co_work_android_uset.R
 import com.example.msigl62.coworkandroiduset.ContractMain
 import com.example.msigl62.coworkandroiduset.PresenterMain
 import com.example.msigl62.coworkandroiduset.base.BaseActivity
+import com.roughike.bottombar.BottomBar
+import com.roughike.bottombar.OnMenuTabClickListener
+
 class HomeActivity : BaseActivity<ContractMain.View, PresenterMain>(){
-
+    private var bottomBar: BottomBar? = null
     override fun setUpBottomBar() {
-
+        val savedInstanceState = null
+        bottomBar = BottomBar.attachShy(findViewById(R.id.myCoordinator),
+                findViewById(R.id.myScrollingContent), savedInstanceState)
+        bottomBar!!.setItems(R.menu.bottom_bar_menu)
+        bottomBar!!.setOnMenuTabClickListener(object : OnMenuTabClickListener {
+            override fun onMenuTabSelected(@IdRes menuItemId: Int) {
+                if (menuItemId==R.id.like){
+                    Toast.makeText(applicationContext, "", Toast.LENGTH_LONG).show()
+                }else{ } }
+            override fun onMenuTabReSelected(@IdRes menuItemId: Int) {}
+        })
     }
 
     override fun showProgressDialog() {}
