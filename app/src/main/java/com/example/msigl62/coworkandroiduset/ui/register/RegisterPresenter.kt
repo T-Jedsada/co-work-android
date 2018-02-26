d664f0f0930c5cbf7695b0c6e1ec651e21f17f03
fluke <flukebevil@gmail.com>
2018-02-26 12:28:30 +0700

    modify RegisterPresenter change equal.() to isNullOrEmpty recheck wrong word and fixed it

--- app/src/main/java/com/example/msigl62/coworkandroiduset/ui/register/RegisterPresenter.kt
+++ app/src/main/java/com/example/msigl62/coworkandroiduset/ui/register/RegisterPresenter.kt
@@ -7,23 +7,21 @@ class RegisterPresenter : RegisterContact.Presenter {
                 "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,64}" + "(" + "\\." +
                 "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,25}" + ")+"
         val matcher = Pattern.compile(validEmail).matcher(model.edit_email)
-        when (matcher.matches()) {
-            matcher.matches().equals("") -> view.contactPresenter("Please check email", "", "", "", "", "")
-            model.edit_name.equals("")->view.contactPresenter("Please check name", "", "", "", "", "")
-            model.edit_email.equals("")->view.contactPresenter("Please check email", "", "", "", "", "")
-            model.edit_password.equals("")->view.contactPresenter("Please check password", "", "", "", "", "")
-            model.edit_re_password.equals("")->view.contactPresenter("Please check Repassword", "", "", "", "", "")
-            model.id_facebook.equals(null)->view.contactPresenter("Please check facebook", "", "", "", "", "")
-            model.path_image.equals("")->view.contactPresenter("Please check image", "", "", "", "", "")
-            !model.edit_re_password.equals(model.edit_password)->view.contactPresenter("Please check repassword", "", "", "", "", "")
+        when {
+            !matcher.matches() -> view.contactPresenter("Please check email", "", "", "", "", "")
+            model.edit_name.isNullOrEmpty()->view.contactPresenter("Please check name", "", "", "", "", "")
+            model.edit_email.isNullOrEmpty()->view.contactPresenter("Please check email", "", "", "", "", "")
+            model.edit_password.isNullOrEmpty()->view.contactPresenter("Please check password", "", "", "", "", "")
+            model.edit_re_password.isNullOrEmpty()->view.contactPresenter("Please check RePassword", "", "", "", "", "")
+            model.id_facebook.isNullOrEmpty()->view.contactPresenter("Please check facebook", "", "", "", "", "")
+            model.path_image.isNullOrEmpty()->view.contactPresenter("Please check image", "", "", "", "", "")
+            !model.edit_re_password.equals(model.edit_password)->view.contactPresenter("Please check RePassword", "", "", "", "", "")
             model.edit_name?.length ?: 0>30->view.contactPresenter("Please check length name", "", "", "", "", "")
             model.edit_password?.length ?: 0>6->view.contactPresenter("Please check length password", "", "", "", "", "")
-            model.edit_re_password?.length ?: 0>6->view.contactPresenter("Please check length repassword", "", "", "", "", "")
-            model.path_image.equals(null)->view.contactPresenter("Please check No Image", "", "", "", "", "")
+            model.edit_re_password?.length ?: 0>6->view.contactPresenter("Please check length RePassword", "", "", "", "", "")
+            model.path_image.isNullOrEmpty()->view.contactPresenter("Please check No Image", "", "", "", "", "")
             else -> {
                 view.contactPresenter("true", model.id_facebook, model.edit_name, model.edit_email, model.edit_password, model.path_image)
             } }
     }
 }
-
-
